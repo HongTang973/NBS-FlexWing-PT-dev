4b37bed8aa5e0577252197123585802a85f46a94
Christopher Szczyglowski <cs1414@bristol.ac.uk>
2020-10-09 10:53:27 +0100

    Add functions to setup sandbox environment

---
+++ rmsandbox.m
@@ -0,0 +1,16 @@
+function rmsandbox()
+%rmsandbox  Uninstall sandbox
+%
+%  See also: rmsandbox, 
+%            modify_sandbox_path
+%
+%  Copyright 2016 The MathWorks, Inc.
+%
+% Edits by Christopher Szczyglowski, University of Bristol, 2020
+%   - Refactored most of the code into 'modify_sandbox_path'
+
+% sub_directory_to_remove = {'tbx' ; 'tests' ; 'modules' ; 'examples' ; 'dev'};
+sub_directory_to_remove = {'tests' ; 'examples'};
+modify_sandbox_path(sub_directory_to_remove, 'remove');
+
+end
