782cc124e7896d064e5cae9b6c9ba6b8d4f74f0c
Christopher Szczyglowski <cs1414@bristol.ac.uk>
2020-10-09 12:25:40 +0100

    Add additional example-based tests

--- examples/example_1_pre_bend_example.m
+++ examples/example_1_pre_bend_example.m
@@ -1,14 +1,14 @@
-%% example_1_run_a_test
+%% example_1_pre_bend_example
 %
 %
 %
 
 %Make a model
-% ModelDefinition = parameter_sets.testCase_ClampedPatilHodgesWing;
 ModelDefinition = parameter_sets.testCase_JA_preBendExample;
 
 %Run an analysis
 AnalysisOutput = runSim(0, 1, 'analysisType', 'static', 'fromObject', ModelDefinition);
 
 %Post process the results
-AnalysisOutput.generate_2dplot('JA_preBendExample', {'t',1,'1','1:nt'}, {'Gamma_G', 3, 'ns', '1:nt'});
+part_name = AnalysisOutput.flexParts_nonlinear_cell{1}.partName;
+AnalysisOutput.generate_2dplot(part_name, {'t',1,'1','1:nt'}, {'Gamma_G', 3, 'ns', '1:nt'});
