f9dee639f486c8c5d39a8dce8bf851b1b62c20bb
James Ascham <james.ascham@bristol.ac.uk>
2020-11-08 13:40:31 +0000

    Updated modify_sandbox_path.m for nested sandbox functions
    Utilise nested sandbox functions within dpd subdirectories of the main toolbox.

--- modify_sandbox_path.m
+++ modify_sandbox_path.m
@@ -6,7 +6,7 @@ function modify_sandbox_path(folders, option)
 %
 % Copyright 2016 The Mathworks, Inc.
 %
-% Edits by Christopher Szczyglowski, University of Bristol, 2020
+% Edits by Christopher Szczyglowski & James Ascham, University of Bristol, 2020
 
 assert(iscellstr(folders), ['Expected the folders to be provided ', ...
     'as a cell-array of strings.']); %#ok<ISCLSTR>
@@ -17,7 +17,7 @@ package_directory       = fileparts(mfilename('fullpath'));
 package_sub_directories = fullfile(package_directory, folders);
 
 %Include all subdirectories in the 'tbx' folder.
-idx_tbx = contains(package_sub_directories, 'tbx');
+idx_tbx = contains(extractAfter(package_sub_directories, package_directory), 'tbx'); %allows for nesting
 if nnz(idx_tbx) == 1
     tbx_folder = package_sub_directories{idx_tbx};
     contents = dir(tbx_folder);
@@ -34,7 +34,64 @@ else
         'to be only one folder in the package directory containing ', ...
         'the string `tbx`.']);
 end
-folder_path = [package_sub_directories ; tbx_sub_directories];
+
+%Find all subdirectories in the 'dpd' folder.
+idx_dpd = contains(extractAfter(package_sub_directories, package_directory), 'dpd'); %allows for nesting
+if nnz(idx_dpd) == 1
+    dpd_folder = package_sub_directories{idx_dpd};
+    contents = dir(dpd_folder);
+    idx_directory = ...
+        [contents.isdir] & ...
+        ~strcmp({contents.name}, '.' ) & ...
+        ~strcmp({contents.name}, '..' );
+    contents = contents(idx_directory);
+    dpd_sub_directories = strcat({contents.folder}, filesep, {contents.name})';
+elseif nnz(idx_dpd) == 0
+    dpd_sub_directories = [];
+else
+    error(['Ambigious match for the `dpd` directory. Expected there ', ...
+        'to be only one folder in the package directory containing ', ...
+        'the string `dpd`.']);
+end
+
+folder_path = [package_sub_directories ; tbx_sub_directories]; %do not add dpd_sub_directories to the folder_path here
+
+
+% dpd_sub_directories added to the path using dpd sandbox routine independent
+% of toolbox, if not found then added to folder_path.
+switch option
+    case 'add'
+        for i = 1:length(dpd_sub_directories)
+            contents = dir(dpd_sub_directories{i});
+            fnames = {contents(~[contents.isdir]).name};
+            addSand = any(strcmp(fnames, 'addsandbox.m'));
+            
+            if addSand
+                run(strcat(dpd_sub_directories{i},'\addsandbox.m'));
+            else
+                subfolder_path = genpath(dpd_sub_directories{i});
+                folder_path = [folder_path; subfolder_path];
+                warning(['No addsandbox routine in ',...
+                    dpd_sub_directories{i},...
+                    ': adding all to path. ',...
+                    'Check to avoid conflicts.'])
+            end
+        end
+        
+    case 'remove'
+        for i = 1:length(dpd_sub_directories)
+            contents = dir(dpd_sub_directories{i});
+            fnames = {contents(~[contents.isdir]).name};
+            rmSand = any(strcmp(fnames, 'rmsandbox.m'));
+            
+            if rmSand
+                run(strcat(dpd_sub_directories{i},'\rmsandbox.m'));
+            else
+                subfolder_path = genpath(dpd_sub_directories{i});
+                folder_path = [folder_path; subfolder_path];
+            end
+        end
+end
 
 % Capture path
 oldPathList = path();
